c184bda94c9ea458a101163b95c84124480d67dc
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-11-01 20:26:40 -0300

    Mustache

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import express, {Request, Response} from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
+import mainRoutes from './routes/index';
 
 const server = express();
 
-server.get('/', (req: Request, res:Response)=>{
-    res.send('Olá Mundo!');
+//INCLUINDO O MUSTACHE
+server.set('view engine', 'mustashe');
+server.set('views', path.join(__dirname, 'views')); //Arquivo html
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(mainRoutes);
+
+//Para página não encontrada
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada!');
 
 });
+
 
 server.listen(80);
