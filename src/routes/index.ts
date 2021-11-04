b55aebdec754ab9262c3c659d833d9caf1da66df
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-11-04 14:02:26 -0300

    tabelas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,33 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response)=>{
+  let age: number = 15;
+  let showOld: boolean = false;
+  
+  if(age > 50){
+      showOld = true;
+  }
     // pegar os produtos do banco de dados
     // organizar as informações desses produtos
     // envia para o template engine
-  res.render('home.mustache');
+    //res.render integra o código html
+  res.render('home.mustache', {
+    name: 'Joao',
+    lastName: 'Augusto',
+    age: 90,
+    showOld,
+    products: [
+      {title: 'Produto X', price: 10},
+      {title: 'Produto Y', price: 15},
+      {title: 'Produto W', price: 20}
+    ],
+    frasesDoDia: [
+      'Alguma coisa muito legal',
+      'Outra frase qualquer'
+
+    ]
+    
+  });
 });
 
 router.get('/contato', (req: Request, res: Response)=>{
